b293499de96d3965b09c38d7a151fddb88f38151
farah1987 <farah.mahmood2018@gmail.com>
2023-03-07 13:57:51 +0000

    add secure pwd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase} # self refer each object to the user class
   has_many :articles
-<<<<<<< Updated upstream
-=======
   validates :username, presence: true, uniqueness: { case_sensitive: false} , length:{minimum: 3,maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false}, length:{maximum: 105 },format: {with: VALID_EMAIL_REGEX}
->>>>>>> Stashed changes
+  has_secure_password
 end
